c0c70db9b15ba6a1aba11165118f872b808d8421
Henrymenez <chidinmakiara@gmail.com>
2024-06-19 18:49:57 +0100

    Add project files.

---
+++ Controllers/UrlShortenerController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UrlShortener.Data;
+using UrlShortener.Models;
+
+namespace UrlShortener.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UrlShortenerController : ControllerBase
+    {
+        private readonly UrlShortenerContext _context;
+
+        public UrlShortenerController(UrlShortenerContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ShortenUrl([FromBody] string originalUrl)
+        {
+            if (string.IsNullOrEmpty(originalUrl))
+            {
+                return BadRequest("Invalid URL.");
+            }
+
+            var shortUrl = GenerateShortUrl();
+            var urlMapping = new UrlMapping { OriginalUrl = originalUrl, ShortUrl = shortUrl };
+
+            //   _context.UrlMappings.Add(urlMapping);
+            // await _context.SaveChangesAsync();
+
+            return Ok(new { ShortUrl = shortUrl });
+        }
+
+        [HttpGet("{shortUrl}")]
+        public async Task<IActionResult> RedirectToUrl(string shortUrl)
+        {
+            var urlMapping = new
+            {
+                OriginalUrl = ""
+            };
+            // await _context.UrlMappings
+            // .FirstOrDefaultAsync(u => u.ShortUrl == shortUrl);
+
+            if (urlMapping == null)
+            {
+                return NotFound();
+            }
+
+            return Redirect(urlMapping.OriginalUrl);
+        }
+
+        private string GenerateShortUrl()
+        {
+            var chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            var random = new Random();
+            return new string(Enumerable.Repeat(chars, 6)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+    }
+}
---
+++ Data/UrlShortenerContext.cs
@@ -0,0 +1,13 @@
+﻿using UrlShortener.Models;
+
+namespace UrlShortener.Data
+{
+    public class UrlShortenerContext 
+    {
+       /* public UrlShortenerContext(DbContextOptions<UrlShortenerContext> options) : base(options)
+        {
+        }
+
+        public DbSet<UrlMapping> UrlMappings { get; set; }*/
+    }
+}
